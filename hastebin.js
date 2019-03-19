30af8df912aaad698102aab517b34845b91d2a0a
Sam Gilburg <centumrazor@gmail.com>
2019-03-19 11:59:36 -0700

    :recycle: Refactored to use node-fetch

--- hastebin.js
+++ hastebin.js
@@ -1,15 +1,11 @@
-const rp = require('request-promise');
+const fetch = require('node-fetch');
 
 module.exports = (input, ext) => {
-  const ops = {
-    method: 'POST',
-    uri: 'https://hasteb.in/documents',
-    body: input,
-    json: true,
-  };
   return new Promise((res, rej) => {
-    if (!input) rej(new Error('You need to input something'));
-    rp(ops).then(body => res(`https://hasteb.in/${body.key + (ext ? `.${ext}` : '')}`)).catch(err => rej(err));
+    if (!input) rej(new Error('You need to input something.'));
+    fetch('https://hasteb.in/documents', { method: 'POST', body: input })
+      .then(body => body.json().then(json => res(`https://hasteb.in/${json.key}${(ext) ? `.${ext}` : ''}`)))
+      .catch(err => rej(err));
   });
 };
 // i prefer hasteb.in to hastebin which is honestly the only reason this exists
